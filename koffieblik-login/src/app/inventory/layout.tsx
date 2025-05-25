014926ca53dbae06bc69ca563e346bc3704f8583
JuanFerreira22491504 <u22491504@tuks.co.za>
2025-05-25 19:41:55 +0200

    Added styling to inventory menu

--- koffieblik-login/src/app/inventory/layout.tsx
+++ koffieblik-login/src/app/inventory/layout.tsx
@@ -9,10 +9,10 @@ export default function InventoryLayout({ children }: { children: React.ReactNod
         <h2 className="font-semibold mb-2">Inventory Menu</h2>
         <ul>
           <li>
-            <Link href="/inventory">Overview</Link>
+            <Link href="/inventory" className="block px-3 py-2 rounded hover:bg-gray-100">Overview</Link>
           </li>
           <li>
-            <Link href="/inventory/add">Add Item</Link>
+            <Link href="/inventory/add"  className="block px-3 py-2 rounded hover:bg-gray-100">Add Item</Link>
           </li>
           {}
         </ul>
