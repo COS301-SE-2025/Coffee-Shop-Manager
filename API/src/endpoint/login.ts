4bc26d16a0ef4477757b7a8aab8953e36794990b
u23543834-Emil <u23543834@tuks.co.za>
2025-08-15 00:49:06 +0200

    API: login endpoint stores refresh_token aswell now, added new response template waiting on FE to update functionality

--- API/src/endpoint/login.ts
+++ API/src/endpoint/login.ts
@@ -17,23 +17,33 @@ export async function loginHandler(req: Request, res: Response): Promise<void> {
       return;
     }
 
-    const token = data.session.access_token;
-    const username = data.user.email ?? 'unknown';
-    const maxAge = 60 * 60; // 1 hour
-    const isProd = process.env.NODE_ENV === 'production';
+    const { access_token, refresh_token } = data.session;
 
-    res.setHeader('Set-Cookie', [
-      `token=; Path=/; HttpOnly; Max-Age=0; ${isProd ? 'Secure;' : ''} SameSite=${isProd ? 'Strict' : 'Lax'}`,
-      `token=${token}; Path=/; HttpOnly; Max-Age=${maxAge}; ${isProd ? 'Secure;' : ''} SameSite=${isProd ? 'Strict' : 'Lax'}`,
-    ]);
+    res.cookie('access_token', access_token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 60 * 60 * 1000 // 1 hour
+    });
+    res.cookie('refresh_token', refresh_token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 10 * 365 * 24 * 60 * 60 * 1000 // long time
+    });
 
 
 
+
+    // res.status(200).json({
+    //   success: true,
+    //   username: data.user.user_metadata.display_name || "Mr. Bean",
+    //   role: data.user.user_metadata.role
+    // });
+
+    // old response
     res.status(200).json({
       success: true,
       user: data.user,
-      username, // so that client side can set username to localstorage
-      session: data.session,
+      username: data.user.user_metadata.display_name || "Mr. Bean",
     });
   } catch (err) {
     console.error('Login error:', err);
