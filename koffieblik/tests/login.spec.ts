8caf7bd7d12b0d6c38baf046abfc1f31d003f767
u22867377 <u22867377@tuks.co.za>
2025-08-18 10:25:42 +0200

    added e2e testing for admin

--- koffieblik/tests/login.spec.ts
+++ koffieblik/tests/login.spec.ts
@@ -1,10 +1,26 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Login Flow', () => {
-    test('Login works with valid credentials', async ({ page }) => {
+
+    // user test
+    test('Login works with valid credentials user', async ({ page }) => {
         await page.goto('http://localhost:3000/login');
-        await page.fill('#email', 'test@example.com');     
+        await page.fill('#email', 'user9@coffee.com');
         await page.fill('#password', 'P@ssword123');
+
+        await Promise.all([
+            page.waitForURL('**/userdashboard'),
+            page.click('button[type="submit"]'),
+        ]);
+
+        await expect(page).toHaveURL(/.*userdashboard.*/);
+        await expect(page.getByRole('heading', { name: /dashboard/i })).toBeVisible();
+    });
+
+    test('Login works with valid credentials admin', async ({ page }) => {
+        await page.goto('http://localhost:3000/login');
+        await page.fill('#email', 'admin@coffee.com');
+        await page.fill('#password', 'admin');
 
         await Promise.all([
             page.waitForURL('**/dashboard'),
@@ -21,7 +37,7 @@ test.describe('Login Flow', () => {
         await page.fill('#password', 'WrongPas@sword123');
         await page.click('button[type="submit"]');
 
-        
+
         await expect(page.locator('text=/Invalid login credentials/i')).toBeVisible();
     });
 });
