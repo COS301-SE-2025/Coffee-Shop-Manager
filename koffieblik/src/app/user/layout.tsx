cb32bef2fcedd02df4506f001bb6cba38fc584ac
JuanFerreira22491504 <u22491504@tuks.co.za>
2025-09-27 21:30:28 +0200

    Fixed user page errors

--- koffieblik/src/app/user/layout.tsx
+++ koffieblik/src/app/user/layout.tsx
@@ -20,22 +20,16 @@ export default function UserLayout({
       className="h-screen flex flex-col"
       style={{ backgroundColor: "var(--primary-1)" }}
     >
-      {/* ✅ Top Navbar */}
+      {}
       <Navbar />
 
-      {/* ✅ Main content: full height after navbar */}
+      {}
       <div
         className="flex flex-1 overflow-hidden"
         style={{ color: "var(--primary-3)" }}
       >
-        
 
-         
-            
-            
-         
-
-        {/* ✅ Scrollable content area */}
+        {}
         <div
           className="flex-1 overflow-y-auto p-6"
           style={{ backgroundColor: "var(--primary-2)" }}
