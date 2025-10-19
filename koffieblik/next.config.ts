d0beed9f92d3525c9f5a2e1040f02f15176f0ca1
u23524333(Willem) <u23524333@tuks.co.za>
2025-10-19 19:15:08 +0200

    APK Changes

--- koffieblik/next.config.ts
+++ koffieblik/next.config.ts
@@ -4,6 +4,23 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/app-release.apk',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/vnd.android.package-archive',
+          },
+          {
+            key: 'Content-Disposition',
+            value: 'attachment; filename="DieKoffieBlik.apk"',
+          },
+        ],
+      },
+    ];
+  },
   // other config options here
 };
 
